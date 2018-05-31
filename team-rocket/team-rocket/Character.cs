210950d9a2a6336691fefbbe17dbfa975cd2273d
Tarczok <iah61marczok@gso-koeln.de>
2018-05-31 18:19:03 +0200

    Added hitbox and sprite properties to the character.cs.

--- team-rocket/team-rocket/Character.cs
+++ team-rocket/team-rocket/Character.cs
@@ -10,7 +10,10 @@ namespace team_rocket
 	class Character
 	{
 		private int x, y;
+		private Bitmap[] sprite;
+		private Rectangle hitbox;
 
+		#region Properties
 		/// <summary>
 		/// Sets and returns the X coordinate of the character.
 		/// </summary>
@@ -29,10 +32,29 @@ namespace team_rocket
 			set { y = value; }
 		}
 
+		/// <summary>
+		/// Returns the hitbox of the character.
+		/// </summary>
+		public Rectangle Hitbox
+		{
+			get { return hitbox; }
+		}
+
+		/// <summary>
+		/// Returns the Sprite-Array, which will represent the walking animation of the character later on.
+		/// </summary>
+		public Bitmap[] Sprite
+		{
+			get { return sprite; }
+		}
+		#endregion
+
 		public Character(int x, int y)
 		{
 			X = x;
 			Y = y;
+			hitbox.Height = 64;
+			hitbox.Width = 32;
 		}
 	}
 }
