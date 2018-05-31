eb1b943b26835fd60031b552927d9d8f182c74c2
Tarczok <iah61marczok@gso-koeln.de>
2018-05-31 17:41:02 +0200

    Created the Character-Feature branch and the Character.cs with a basic coordinate structure.

---
+++ team-rocket/team-rocket/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace team_rocket
+{
+	class Character
+	{
+		private int x, y;
+
+		/// <summary>
+		/// Sets and returns the X coordinate of the character.
+		/// </summary>
+		public int X
+		{
+			get { return x; }
+			set { x = value; }
+		}
+
+		/// <summary>
+		/// Sets and returns the Y coordinate of the character.
+		/// </summary>
+		public int Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
+
+		public Character(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
