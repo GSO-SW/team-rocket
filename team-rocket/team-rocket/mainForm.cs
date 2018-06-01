5d1e22f2b43eaf57f463dec73c54a56fb4d950dd
Jurek <jurvoel@gmx.de>
2018-06-01 15:38:50 +0200

    removed unused vars merged both tick-eventhandler added flag-control for the key 'A' and 'D' in OnKeyDown and OnKeyUp added demo-gravity

--- team-rocket/team-rocket/mainForm.cs
+++ team-rocket/team-rocket/mainForm.cs
@@ -10,33 +10,37 @@ using System.Windows.Forms;
 
 namespace team_rocket
 {
-    public partial class main : Form
-    {
-        Timer updateGraphicsTimer;
-        Tile[] tilesArray;
+	public partial class main : Form
+	{
+		Timer updateGraphicsTimer;
+		Tile[] tilesArray;
 		Bitmap[] bitmapArray;
 		Level testLevel;
 		Character character;
-		bool keyAIsPressed, keyDIsPressed;
+		bool bTempA, bNowA;
+		bool bTempD, bNowD;
+		int velocity; //Unit px/tick
 
-        public main()
-        {
-            InitializeComponent();
+		public main()
+		{
+			InitializeComponent();
 
-            ClientSize = new Size(1024, 768); //32*32 | 24*32
+			ClientSize = new Size(1024, 768); //32*32 | 24*32
 
-            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
-            SetStyle(ControlStyles.UserPaint, true);
-            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+			SetStyle(ControlStyles.UserPaint, true);
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			
+			bTempA = bNowA = bTempD = bNowD = false;
+			velocity = 5;
 
-			keyAIsPressed = keyDIsPressed = false;
 
 			#region Initialize Frame Timer
 			// The timer which determines the FPS
 			updateGraphicsTimer = new Timer();
-            updateGraphicsTimer.Interval = 20;
-            updateGraphicsTimer.Tick += UpdateGraphichsTimer_Tick;
-            updateGraphicsTimer.Start();
+			updateGraphicsTimer.Interval = 20;
+			updateGraphicsTimer.Tick += OnTimerTick;
+			updateGraphicsTimer.Start();
 			#endregion
 
 			#region Loading Textures
@@ -50,14 +54,14 @@ namespace team_rocket
 			#region Initialize the tiles
 			tilesArray = new Tile[768];
 			int counter = 0;
-            for (int i = 0; i < 24; i++)
-            {
-                for (int j = 0; j < 32; j++)
-                {
+			for (int i = 0; i < 24; i++)
+			{
+				for (int j = 0; j < 32; j++)
+				{
 					tilesArray[counter] = new Tile(j * 32, i * 32, 0, false);
 					counter++;
-                }
-            }
+				}
+			}
 			#endregion
 
 			#region Test Level
@@ -72,14 +76,12 @@ namespace team_rocket
 
 			loadLevel(testLevel);
 			#endregion
-
-			// Experimental
+			
 			character = new Character(new Point(50, 50));
 			KeyDown += OnKeyDown;
 			KeyUp += OnKeyUp;
-			updateGraphicsTimer.Tick += OnTimerTick;
 
-        }
+		}
 
 		private void loadLevel(Level lvl)
 		{
@@ -89,75 +91,88 @@ namespace team_rocket
 			}
 		}
 
-        /// <summary>
-        /// Event-Handler for the Graphic-Update-Timer-Tick
-        /// </summary>
-        /// <param name="sender">Contains information which object triggered the Event.</param>
-        /// <param name="e">Conatins informatin about the Event.</param>
-        private void UpdateGraphichsTimer_Tick(object sender, EventArgs e)
-        {
-            Invalidate();
-        }
+		/// <summary>
+		/// Event-Handler for the Graphic-Update-Timer-Tick
+		/// </summary>
+		/// <param name="sender">Contains information which object triggered the Event.</param>
+		/// <param name="e">Conatins informatin about the Event.</param>
+		private void OnTimerTick(object sender, EventArgs e)
+		{
+			character.Velocity = new Size(character.Velocity.Width, character.Velocity.Height + 1);
+			if (character.Location.Y > 600)
+			{
+				character.Velocity = new Size(character.Velocity.Width, 0);
+			}
 
-        /// <summary>
-        /// Paint-Event-Handler
-        /// </summary>
-        /// <param name="e">Conatins informatin about the Event.</param>
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
+			character.Location += character.Velocity;
 
-			foreach(Tile item in tilesArray)
+			Invalidate();
+		}
+
+		/// <summary>
+		/// Paint-Event-Handler
+		/// </summary>
+		/// <param name="e">Conatins informatin about the Event.</param>
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+
+			foreach (Tile item in tilesArray)
 			{
 				e.Graphics.DrawImage(bitmapArray[item.ImageID], item.Rect.Location);
 			}
 			e.Graphics.FillRectangle(Brushes.Red, new Rectangle(character.Location, character.Hitbox.Size));
-        }
-
-		/// <summary>
-		/// Eventhandler for every frame tick in the mainForm, handles the movement in X dimension.
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public void OnTimerTick(object sender, EventArgs e)
-		{
-			// Checks if character should be moving
-			if (keyAIsPressed)
-				character.Location = new Point(character.Location.X - 5, character.Location.Y);
-			if (keyDIsPressed)
-				character.Location = new Point(character.Location.X + 5, character.Location.Y);
-
-			//character.Location += character.Velocity;
 		}
 
 		/// <summary>
-		/// Eventhandler whenever a random key is being pressed, handles the X dimension flag.
+		/// Eventhandler whenever a key is being pressed.
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
 		public void OnKeyDown(object sender, KeyEventArgs e)
 		{
-			// The character should be moving, when either A or D is being pressed
 			if (e.KeyCode == Keys.A)
-				keyAIsPressed = true;
-			if (e.KeyCode == Keys.D)
-				keyDIsPressed = true;
+			{
+				// bTempA saves the state (pressed or not) of the Key 'A' from the last execution of the OnKeyDown-EventHandler.
+				// bNpw A saves the state (pressed or not) of the Key 'A' at the moment.
+				bTempA = bNowA; 
+				bNowA = true;
+				// Comparing bNowA and bTempA to detect if the key was being pressed since the last comparison.
+				if (bNowA != bTempA)
+				{
+					//The code which should be executed only one time if the key is being pressed.
+					character.Velocity = new Size(character.Velocity.Width - velocity, character.Velocity.Height);
+				}
+			}
 			
+			if (e.KeyCode == Keys.D) // see above
+			{
+				bTempD = bNowD;
+				bNowD = true;
+				if (bNowD != bTempD)
+				{
+					character.Velocity = new Size(character.Velocity.Width + velocity, character.Velocity.Height);
+				}
+			}
 		}
 
 		/// <summary>
-		/// Eventhandler whenever a key is being left, handles the X dimension flag.
+		/// Eventhandler whenever a key is being let go.
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		public void OnKeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+		public void OnKeyUp(object sender, KeyEventArgs e)
 		{
-			// The character should not be moving, when A or D is stopped being pressed
 			if (e.KeyCode == Keys.A)
-				keyAIsPressed = false;
+			{
+				bNowA = bTempA = false; //bNowA and bTempA will be set to false because the key isn't pressed anymore.
+				character.Velocity = new Size(character.Velocity.Width + velocity, character.Velocity.Height);
+			}
 			if (e.KeyCode == Keys.D)
-				keyDIsPressed = false;
-
+			{
+				bNowD = bTempD = false;
+				character.Velocity = new Size(character.Velocity.Width - velocity, character.Velocity.Height);
+			}
 		}
 	}
 }
